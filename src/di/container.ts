9c3fb8ed3086e35b3916733857ac6b3c63212f85
Luis Adrian Ramos <luchoramos042004@gmail.com>
2025-10-28 17:44:00 -0500

    final

--- src/di/container.ts
+++ src/di/container.ts
@@ -1,14 +1,17 @@
-import { SQLiteTodoDataSource } from '@/src/data/datasources/SQLiteTodoDataSource';
-import { TodoRepositoryImpl } from '@/src/data/repositories/TodoRepositoryImpl';
+// import { SQLiteTodoDataSource } from '@/src/data/datasources/SQLiteTodoDataSource';
+// import { TodoRepositoryImpl } from '@/src/data/repositories/TodoRepositoryImpl';
+
 import { GetAllTodos } from '../domain/usecases/GetTodo';
 import { CreateTodo } from '../domain/usecases/CreateTodo';
 import { ToggleTodo } from '../domain/usecases/ToogleTodo';
 import { DeleteTodo } from '../domain/usecases/DeleteTodo';
+import { FirebaseTodoDataSource } from '../data/datasources/FirebaseTodoDataSource';
+import { TodoRepositoryFirebaseImpl } from '../data/repositories/TodoRepositoryFirebaseImpl';
 
 class DIContainer {
   private static instance: DIContainer;
-  private _dataSource: SQLiteTodoDataSource | null = null;
-  private _repository: TodoRepositoryImpl | null = null;
+  private _dataSource: FirebaseTodoDataSource | null = null;
+  private _repository: TodoRepositoryFirebaseImpl | null = null;
 
   private constructor() { }
 
@@ -20,9 +23,9 @@ class DIContainer {
   }
 
   async initialize(): Promise<void> {
-    this._dataSource = new SQLiteTodoDataSource();
+    this._dataSource = new FirebaseTodoDataSource();
     await this._dataSource.initialize();
-    this._repository = new TodoRepositoryImpl(this._dataSource);
+    this._repository = new TodoRepositoryFirebaseImpl(this._dataSource);
   }
 
   get getAllTodos(): GetAllTodos {
